faef6cfbd8e959c4b093ebecc42e7b6bd93e4d73
Rizky Hamdani Sakti <93897908+Rizkyhamm@users.noreply.github.com>
2023-12-14 20:20:37 +0700

    Add files via upload
    bismillah lagi

--- src/main/java/main/equals.java
+++ src/main/java/main/equals.java
@@ -1,25 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package main;
 
-/**
- *
- * @author Dewi
- */
-
 import java.io.File;
-import java.net.URL;
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class equals extends Application{
-@Override
+import java.io.IOException;
+import java.net.URL;
+import static javafx.application.Application.launch;
+
+/**
+ * JavaFX App
+ */
+public class equals extends Application {
+
+    private static Scene scene;
+
+   @Override
     public void start(Stage stage) throws Exception {
         URL url = new File("src/main/java/view/Homepage.fxml").toURI().toURL();
         Parent root = FXMLLoader.load(url);
@@ -28,10 +27,12 @@ public class equals extends Application{
         stage.setTitle("equals");
         stage.show();
     }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
     }
+
 }
