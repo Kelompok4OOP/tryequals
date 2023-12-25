5d416e76776d7e41672a3433e46c8e6d68085fc6
Rizky Hamdani Sakti <93897908+Rizkyhamm@users.noreply.github.com>
2023-12-25 12:29:34 +0700

    Add files via upload
    till xmas

--- src/main/java/controller/HomepageController.java
+++ src/main/java/controller/HomepageController.java
@@ -4,9 +4,17 @@
  */
 package controller;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,6 +23,13 @@ import javafx.fxml.Initializable;
  */
 public class HomepageController implements Initializable {
 
+    @FXML
+    private Button btnAboutUs;
+    @FXML
+    private Button btnLogin;
+    @FXML
+    private Button btnSignup;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +37,41 @@ public class HomepageController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void handleAboutUs(ActionEvent event) {
+        /*
+        URL url = new File("src/main/java/view/Login.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Online Diary");
+        stage.show();
+        */
+    }
+
+    @FXML
+    private void handleLogin(ActionEvent event) {
+        /*
+        URL url = new File("src/main/java/view/Login.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Login");
+        stage.show();
+        */
+    }
+
+    @FXML
+    private void handleSignUp(ActionEvent event) {
+        /*
+        URL url = new File("src/main/java/view/Signup.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Signup");
+        stage.show();
+        */
+    }
     
 }
